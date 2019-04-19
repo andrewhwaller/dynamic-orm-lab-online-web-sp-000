e452895c3b2a6fdfdc455cb2974d752701967d5d
Andrew Waller <andrewhwaller@gmail.com>
2019-04-19 22:16:11 +0000

    Automatically backed up by Learn

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -6,8 +6,8 @@ describe Student do
 
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY, 
-      name TEXT, 
+      id INTEGER PRIMARY KEY,
+      name TEXT,
       grade INTEGER
       )
     SQL
